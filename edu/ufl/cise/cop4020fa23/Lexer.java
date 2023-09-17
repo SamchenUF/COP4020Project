d5a1921606701df8c92611ab822c87fa12f7c40a
Sam Chen <76010610+AsianShep@users.noreply.github.com>
2023-09-16 22:05:07 -0400

    Changed back to my code

--- edu/ufl/cise/cop4020fa23/Lexer.java
+++ edu/ufl/cise/cop4020fa23/Lexer.java
@@ -1,379 +1,267 @@
 package edu.ufl.cise.cop4020fa23;
-
-import static edu.ufl.cise.cop4020fa23.Kind.IDENT;
-
+import static edu.ufl.cise.cop4020fa23.Kind.EOF;
+import java.nio.channels.IllegalSelectorException;
 import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Lexer implements ILexer {
-
 	String input;
-	int currentPos = 0;
-	private char[] chars;
-	private int pos = 0;
-
-	private static final Set<String> RESERVED_WORDS = new HashSet<>(Arrays.asList("image", "pixel", "int", "string", "void", "boolean", "write", "height", "width", "if", "fi", "do", "od", "red", "green", "blue"));
-	private static final Set<String> BOOLEAN_LITERALS = new HashSet<>(Arrays.asList("TRUE", "FALSE"));
-	private static final Set<String> CONSTANTS = new HashSet<>(Arrays.asList("Z", "BLACK", "BLUE", "CYAN", "DARK_GRAY", "GRAY", "GREEN", "LIGHT_GRAY", "MAGENTA", "ORANGE", "PINK", "RED", "WHITE", "YELLOW"));
+	int sentinel;
+	int i; //position
+	int row;
+	int column;
+	char[] arr;
+	private enum State {
+		START, HAVE_EQ, HAVE_AND, HAVE_LT, HAVE_GT, HAVE_STAR, HAVE_STRAIGHT, HAVE_LB, HAVE_COLON, HAVE_HASH, HAVE_DASH, HAVE_OR, HAVE_DIGIT;
+	}
 
 	public Lexer(String input) {
 		this.input = input;
-		this.chars = input.toCharArray();
+		sentinel = input.length();
+		i = 0;
+		row = 1;
+		column = 0;
+		arr = input.toCharArray();
 	}
-
-	private boolean isLetter(char c) {
-		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-	}
-
-	private boolean isDigit(char c) {
-		return c >= '0' && c <= '9';
-	}
-
-	private IToken identifier(int pos) {
-		int startPos = pos;
-		pos++;
-		while (pos < input.length() && (isLetter(input.charAt(pos)) || isDigit(input.charAt(pos)) || input.charAt(pos) == '_')) {
-			pos++;
-		}
-		String lexeme = input.substring(startPos, pos);
-		char[] source = lexeme.toCharArray();
-
-		if (RESERVED_WORDS.contains(lexeme)) {
-			Kind kind;
-			switch (lexeme) {
-				case "image":
-					kind = Kind.RES_image;
-					break;
-				case "pixel":
-					kind = Kind.RES_pixel;
-					break;
-				case "int":
-					kind = Kind.RES_int;
-					break;
-				case "string":
-					kind = Kind.RES_string;
-					break;
-				case "void":
-					kind = Kind.RES_void;
-					break;
-				case "boolean":
-					kind = Kind.RES_boolean;
-					break;
-				case "write":
-					kind = Kind.RES_write;
-					break;
-				case "height":
-					kind = Kind.RES_height;
-					break;
-				case "width":
-					kind = Kind.RES_width;
-					break;
-				case "if":
-					kind = Kind.RES_if;
-					break;
-				case "fi":
-					kind = Kind.RES_fi;
-					break;
-				case "do":
-					kind = Kind.RES_do;
-					break;
-				case "od":
-					kind = Kind.RES_od;
-					break;
-				case "red":
-					kind = Kind.RES_red;
-					break;
-				case "green":
-					kind = Kind.RES_green;
-					break;
-				case "blue":
-					kind = Kind.RES_blue;
-					break;
-				default:
-					throw new RuntimeException("Unexpected reserved word: " + lexeme);
-			}
-			currentPos = pos;  // Update the current position here
-			return new Token(kind, startPos, pos-startPos, input.toCharArray(), new SourceLocation(1, 1));
-		} else if (BOOLEAN_LITERALS.contains(lexeme)) {
-			currentPos = pos;  // Update the current position here
-			return new Token(Kind.BOOLEAN_LIT, startPos, pos-startPos, input.toCharArray(), new SourceLocation(1, 1));
-		} else if (CONSTANTS.contains(lexeme)) {
-			currentPos = pos;  // Update the current position here
-			return new Token(Kind.CONST, startPos, pos-startPos, input.toCharArray(), new SourceLocation(1, 1));
-		}
-		currentPos = pos;  // Update the current position here
-		return new Token(IDENT, startPos, pos-startPos, input.toCharArray(), new SourceLocation(1, 1));
-	}
-
-	private IToken number(int pos) {
-		int startPos = pos;
-		int length = 1;
-		if (input.charAt(pos) == '0') {
-			pos++;
-			currentPos = pos;  // Update the current position here
-			return new Token(Kind.NUM_LIT, startPos, 1, input.toCharArray(), new SourceLocation(1, 1));
-		}
-		pos++;
-		while (pos < input.length() && isDigit(input.charAt(pos))) {
-				pos++;
-				length++;
-			}
-		//String lexeme = input.substring(startPos, pos);
-		//char[] lexemeCharArray = lexeme.toCharArray();
-		currentPos = pos;  // Update the current position here
-		return new Token(Kind.NUM_LIT, startPos, length, input.toCharArray(), new SourceLocation(1, 1));
-	}
-	private IToken stringLit(int pos) throws LexicalException {
-		int startPos = pos;
-		pos++;
-		while (pos < input.length() && input.charAt(pos) != '"') {
-			pos++;
-		}
-		if (pos >= input.length()) {
-			throw new LexicalException("Unclosed string literal", pos);
-		}
-		pos++;
-		//String lexeme = input.substring(startPos, pos);
-		//char[] lexemeCharArray = lexeme.toCharArray();
-		currentPos = pos;  // Update the current position here
-		return new Token(Kind.STRING_LIT, startPos, pos-startPos, input.toCharArray(), new SourceLocation(1, 1));
-	}
-
-
-	private int skipWhiteSpaceAndComments(int pos) {
-		boolean commentsOrWhitespaceFound;
-		do {
-			commentsOrWhitespaceFound = false;
-
-			// Skip white space
-			while (pos < input.length() && Character.isWhitespace(input.charAt(pos))) {
-				pos++;
-			}
-
-			// Skip comments
-			if (pos < input.length() - 1 && input.charAt(pos) == '#' && input.charAt(pos + 1) == '#') {
-				commentsOrWhitespaceFound = true;
-				pos += 2;  // Skip the "##"
-				while (pos < input.length() && input.charAt(pos) != '\n') {
-					pos++;
-				}
-			}
-
-			// This loop will continue until we find a position that is neither white space nor the start of a comment.
-		} while (commentsOrWhitespaceFound && pos < input.length());
-
-		return pos;
-	}
-
-
-	private boolean match(int pos, String pattern) {
-		for (int i = 0; i < pattern.length(); i++) {
-			if (pos + i >= chars.length || chars[pos + i] != pattern.charAt(i)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	class TokenResult {
-		Token token;
-		int newPos;
-
-		TokenResult(Token token, int newPos) {
-			this.token = token;
-			this.newPos = newPos;
-		}
-
-		Token getToken() {
-			return token;
-		}
-
-		int getNewPos() {
-			return newPos;
-		}
-
-		int getEndPos() {
-			return newPos;
-		}
-	}
-
-
-	public TokenResult operatorOrSeparator(int pos) throws LexicalException {
-		char ch = chars[pos];
-		String lexeme = String.valueOf(ch);
-		Kind kind = null;
-		int lengthOfToken = 0;
-		switch (ch) {
-			case ','-> {
-				kind = Kind.COMMA;
-				lengthOfToken = 1;
-			}
-			case ';' -> {
-				kind = Kind.SEMI;
-				lengthOfToken = 1;
-			}
-			case '?' -> {
-				kind = Kind.QUESTION;
-				lengthOfToken = 1;
-			}
-			case ':' -> {
-				if (match(pos, ":>")) {
-					lexeme += '>';
-					kind = Kind.BLOCK_CLOSE;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.COLON;
-					lengthOfToken = 1;
-				}
-			}
-			case '(' -> {
-				kind = Kind.LPAREN;
-				lengthOfToken = 1;
-			}
-			case ')' -> {
-				kind = Kind.RPAREN;
-				lengthOfToken = 1;
-			}
-			case '<' -> {
-				if (match(pos, "<=")) {
-					lexeme += '=';
-					kind = Kind.LE;
-					lengthOfToken = 2;
-				} else if (match(pos, "<:")) {
-					lexeme += ':';
-					kind = Kind.BLOCK_OPEN;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.LT;
-					lengthOfToken = 1;
-				}
-			}
-			case '>' -> {
-				if (match(pos, ">=")) {
-					lexeme += '=';
-					kind = Kind.GE;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.GT;
-					lengthOfToken = 1;
-				}
-			}
-			case '[' -> {
-				if (match(pos, "[]")) {
-					lexeme += ']';
-					kind = Kind.BOX;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.LSQUARE;
-					lengthOfToken = 1;
-				}
-			}
-			case ']' -> {
-				kind = Kind.RSQUARE;
-				lengthOfToken = 1;
-			}
-			case '=' -> {
-				if (match(pos, "==")) {
-					lexeme += '=';
-					kind = Kind.EQ;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.ASSIGN;
-					lengthOfToken = 1;
-				}
-			}
-			case '!' -> {
-				kind = Kind.BANG;
-				lengthOfToken = 1;
-			}
-			case '&' -> {
-				if (match(pos, "&&")) {
-					lexeme += '&';
-					kind = Kind.AND;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.BITAND;
-					lengthOfToken = 1;
-				}
-			}
-			case '|' ->  {
-				if (match(pos, "||")) {
-					lexeme += '|';
-					kind = Kind.OR;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.BITOR;
-					lengthOfToken = 1;
-				}
-			}
-			case '+' -> {
-				kind = Kind.PLUS;
-				lengthOfToken = 1;
-			}
-			case '-' -> {
-				if (match(pos, "->")) {
-					lexeme += '>';
-					kind = Kind.RARROW;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.MINUS;
-					lengthOfToken = 1;
-				}
-			}
-			case '*' -> {
-				if (match(pos, "**")) {
-					lexeme += '*';
-					kind = Kind.EXP;
-					lengthOfToken = 2;
-				} else {
-					kind = Kind.TIMES;
-					lengthOfToken = 1;
-				}
-			}
-			case '/' -> {
-				kind = Kind.DIV;
-			}
-			case '%' -> {
-				kind = Kind.MOD;
-			}
-			case '^' -> {
-				kind = Kind.RETURN;
-				lengthOfToken = 1;
-			}
-			default ->
-				throw new LexicalException("Illegal character", pos);
-		}
-
-		return new TokenResult(new Token(kind, pos, lexeme.length(), input.toCharArray(), new SourceLocation(1, 1)), pos + lexeme.length());
-	}
-
-
-
-
-
 	@Override
 	public IToken next() throws LexicalException {
-		currentPos = skipWhiteSpaceAndComments(currentPos);  // <--- Start from the current position
-
-		if (currentPos >= input.length()) {
-			return new Token(Kind.EOF, currentPos, currentPos, "EOF".toCharArray(), new SourceLocation(1, 1));
+		State state = State.START;
+		int startPos = i;
+		boolean temp = true;
+		boolean newTok = true;
+		if (sentinel == 0) {
+			temp = false;
 		}
+		if (i >= sentinel) {
+				temp = false;
+			}
+		while(temp) {
+			char current = input.charAt(i);
+			if(newTok) {startPos = i;}
+			column++;
 
-		char c = input.charAt(currentPos);
-		if (isLetter(c)) {
-			return identifier(currentPos);
-		} else if (isDigit(c)) {
-			return number(currentPos);
-		} else if (c == '"') {
-			return stringLit(currentPos);
-		} else {
-			TokenResult result = operatorOrSeparator(currentPos);
-			currentPos = result.getEndPos();  // Update currentPos after tokenizing
-			return result.getToken();
+			switch (state) {
+
+				case START -> {
+
+					switch(current) {
+
+						case ' ', '\n', '\r' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.NUM_LIT, startPos, 1, arr, new SourceLocation(row, column));}
+							else {newTok = false; state = State.HAVE_DIGIT;}
+						} 
+						case '0' -> {
+							i++;
+							state = State.HAVE_DIGIT;
+						}
+						case '+' -> {
+							i++;
+							return new Token(Kind.PLUS, startPos, 1, arr, new SourceLocation(row, i));
+						}
+						case ',' -> {
+							i++;
+							return new Token(Kind.COMMA, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case ';' -> {
+							i++;
+							return new Token(Kind.SEMI, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case '?' -> {
+							i++;
+							return new Token(Kind.QUESTION, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case ':' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.COLON, startPos, 1, arr, new SourceLocation(row, column));}
+							else {newTok = false; state = State.HAVE_COLON;}
+						}
+						case '(' -> {
+							i++;
+							return new Token(Kind.LPAREN, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case ')' -> {
+							i++;
+							return new Token(Kind.RPAREN, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case '[' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.LSQUARE, startPos, 1, arr, new SourceLocation(row, column));}
+							else {newTok = false; state = State.HAVE_LB;}
+							
+						}
+						case ']' -> {
+							i++;
+							return new Token(Kind.RSQUARE, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case '<' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.LT, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_LT;}
+							
+						}
+						case '>' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.GT, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_GT;}
+						}
+						case '=' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.ASSIGN, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_EQ;}
+						}
+						case '&'-> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.BITAND, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_AND;}
+						}
+						case '%' -> {
+							i++;
+							return new Token(Kind.MOD, startPos, 1, arr, new SourceLocation(row, column));
+							}
+						case '#' -> {
+							i++;
+							if (input.charAt(i) == '#' ){i++; state = State.HAVE_HASH;}
+							else {throw new IllegalStateException("not comment");}
+						}
+						case '/' -> {
+							i++;
+							return new Token(Kind.DIV, startPos, 1, arr, new SourceLocation(row, column));
+						}
+						case '-' -> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.MINUS, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_DASH;}
+						}
+						case '!' -> {
+							i++;
+							return new Token(Kind.BANG, startPos, 1, arr, new SourceLocation(row, column));
+							}
+						case '|'-> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.BITOR, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_OR;}
+						}
+						case '*'-> {
+							i++;
+							if(i >= sentinel) {return new Token(Kind.TIMES, startPos, 1, arr, new SourceLocation(row, column));}
+							else{newTok = false; state = State.HAVE_STAR;}
+						}
+						default -> {
+							throw new IllegalStateException("lexer bug");
+						}
+					}
+				}
+				case HAVE_STAR -> {
+					newTok = true;
+					switch(current) {
+						case '*' -> {
+							i++;
+							return new Token(Kind.EXP, startPos, 2, arr, new SourceLocation(row, column));
+						}
+						default -> {
+							return new Token(Kind.TIMES, startPos, 1, arr, new SourceLocation(row, column-1));
+						}
+					}
+				}
+				case HAVE_OR -> {
+					newTok = true;
+					switch(current) {
+						case '|' -> {
+							i++;
+							//state = State.START;
+							return new Token(Kind.OR, startPos, 2, arr, new SourceLocation(row, column));
+						}
+						default -> {
+							//state = State.START;
+							return new Token(Kind.BITOR, startPos, 1, arr, new SourceLocation(row, column-1));
+						}
+					}
+				}
+				case HAVE_DASH -> {
+					newTok = true;
+					if(current == '>') {
+						i++;
+						return new Token(Kind.RARROW, startPos, current, arr, new SourceLocation(row, column));
+					}
+					else {
+						//state = State.START;
+						return new Token(Kind.MINUS, startPos, current, arr, new SourceLocation(row, column));
+					}
+				}
+				case HAVE_GT -> {
+					newTok = true;
+					if(current == '=') {
+						i++;
+						return new Token(Kind.GE, startPos, current, arr, new SourceLocation(row, column));
+					}
+					else {return new Token(Kind.GT, startPos, current, arr, new SourceLocation(row, column));}
+				}
+				case HAVE_LT -> {
+					newTok = true;
+					if(current == '=') {
+						i++;
+						return new Token(Kind.LE, startPos, current, arr, new SourceLocation(row, column));
+					}
+					else if(current == ':') {
+						i++;
+						return new Token(Kind.BLOCK_OPEN, startPos, current, arr, new SourceLocation(row, column));
+					}
+					else{return new Token(Kind.LT, startPos, current, arr, new SourceLocation(row, column));}
+				}
+				case HAVE_HASH -> {
+					if (current == '\n') {newTok = true; state = State.START;}
+					i++;
+					}
+				case HAVE_EQ -> {
+					newTok = true;
+					switch(current) {
+						case '=' -> {
+							i++;
+							//state = State.START;
+							return new Token(Kind.EQ, startPos, 2, arr, new SourceLocation(row, column));
+						}
+						default -> {
+							//state = State.START;
+							return new Token(Kind.ASSIGN, startPos, 1, arr, new SourceLocation(row, column-1));
+						}
+					}
+				}
+				case HAVE_AND -> {
+					newTok = true;
+					switch(current) {
+						case '&' -> {
+							i++;
+							//state = State.START;
+							return new Token(Kind.AND, startPos, 2, arr, new SourceLocation(row, column));
+						}
+						default -> {
+							//state = State.START;
+							return new Token(Kind.BITAND,startPos, 1, arr, new SourceLocation(row, column-1));
+						}
+					}
+				}
+				case HAVE_LB -> {
+					newTok = true;
+					switch(current) {
+						case ']' -> {
+							i++;
+							//state = State.START;
+							return new Token(Kind.BOX, startPos, 2, arr, new SourceLocation(row, column));
+						}
+						default -> {
+							//state = State.START;
+							return new Token(Kind.LSQUARE, startPos, 1, arr, new SourceLocation(row, column-1));
+						}
+					}
+				}
+				default -> {
+					throw new IllegalStateException("lexer bug");
+				}
+			}
+			if (i >= sentinel) {
+				temp = false;
+			}
 		}
+		return new Token(EOF, 0, 0, null, new SourceLocation(1, 1));
 	}
-
-
 }
